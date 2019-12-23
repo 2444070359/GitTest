58af03d0840eb4b7a756d7e9bc48514da8b38dee
HZQ <HZQ@192.168.56.1>
2019-12-23 19:05:42 +0800

    添加一次消息

--- src/cn/blb/UserController.java
+++ src/cn/blb/UserController.java
@@ -5,6 +5,7 @@ public class UserController {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("123131");
+		System.out.println("提交的第一次");
 
 	}
 
